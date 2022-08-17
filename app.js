2a768956811875c4b1f062ff47458e5757c9c963
cwong-scw <cwong@securecodewarrior.com>
2022-08-17 14:06:43 +1000

    Update app.js

--- app.js
+++ app.js
@@ -3,19 +3,10 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/foo/:id', (req, res) => {
+app.get('/foo/:bar', (req, res) => {
     // #1
-    eval('console.log("something", ' + req.params.id + ')');
-    // #2
-    eval('console.log("something else", ' + req.params.id + ')');
-    // #3
-    eval(`console.log("something else", ${req.params.id})`);
-    // #4
-    eval(`console.log("and another one ${req.params.id}")`);
-    // #5
-    eval(`console.log("yet another one ${req.params.id}")`);
-    eval(req.params.id);
-    res.send(`Hello ${req.params.id}`)
+    eval('console.log("something", ' + req.params.bar + ')');
+    res.send(`Hello ${req.params.bar}`)
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
